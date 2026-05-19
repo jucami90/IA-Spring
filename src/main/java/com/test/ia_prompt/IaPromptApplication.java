f16c344f5c6f2d8bc582fdf78c552b5df03e0aa4
jucami90 <jucami90@gmail.com>
2026-05-19 12:22:34 -0300

    add project SPRING AI with openia

---
+++ src/main/java/com/test/ia_prompt/IaPromptApplication.java
@@ -0,0 +1,13 @@
+package com.test.ia_prompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaPromptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaPromptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/ia_prompt/IaPromptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.ia_prompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaPromptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
