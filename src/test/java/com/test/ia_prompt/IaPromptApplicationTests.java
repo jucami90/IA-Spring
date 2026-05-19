14668c1805fd46d8302a1621b5e1f1ae69b8e5bb
jucami90 <jucami90@gmail.com>
2026-05-19 12:39:41 -0300

    add test and enviromental variable

--- src/test/java/com/test/ia_prompt/IaPromptApplicationTests.java
+++ src/test/java/com/test/ia_prompt/IaPromptApplicationTests.java
@@ -1,13 +1,55 @@
 package com.test.ia_prompt;
 
+import com.test.ia_prompt.record.Question;
+import com.test.ia_prompt.service.SpringAiService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class IaPromptApplicationTests {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.wiremock.spring.ConfigureWireMock;
+import org.wiremock.spring.EnableWireMock;
 
-	@Test
-	void contextLoads() {
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+@EnableWireMock(
+		@ConfigureWireMock(baseUrlProperties = "openai.base.url"))
+@SpringBootTest(
+		properties = "spring.ai.openai.base-url=${openai.base.url}")
+public class IaPromptApplicationTests {
+
+	@Value("classpath:/test-openai-response.json")
+	Resource responseResource;
+
+	@Autowired
+	ChatClient.Builder chatClientBuilder;
+
+	@BeforeEach
+	public void setup() throws IOException {
+		var cannedResponse =
+				responseResource.getContentAsString(Charset.defaultCharset());
+		var mapper = new ObjectMapper();
+		var responseNode = mapper.readTree(cannedResponse);
+		WireMock.stubFor(WireMock.post("/v1/chat/completions")
+				.willReturn(ResponseDefinitionBuilder.okForJson(responseNode)));
 	}
 
+	@Test
+	public void testAskQuestion() {
+		var boardGameService =
+				new SpringAiService(chatClientBuilder);
+		var answer =
+				boardGameService.askQuestion(
+						new Question("What is the capital of France?"));
+		Assertions.assertThat(answer).isNotNull();
+		Assertions.assertThat(answer.answer()).isEqualTo("Paris");
+	}
 }
